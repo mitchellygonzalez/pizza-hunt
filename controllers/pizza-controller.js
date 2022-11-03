124ef168fc92c56c3133ecc35d64999e397dd3f1
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-11-03 00:02:31 -0400

    updating

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,20 +1,15 @@
 const { Pizza } = require('../models');
 
 const pizzaController = {
-    // get all pizzas  - GET / api/pizzas   (getAllPizza() uses the following foute. It uses the Mongoose .find() method like Sequelize .findAll method)
-    getAllPizza(req, res) {
-      Pizza.find({})
-        .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
-  
-    // get one pizza by id  (.getPizzaById() uses the Mongoose .findOne() method to find a single pizza by its _id. Instead of accessing the entire req, we've destructured params out of it, because that's the only data we need for this request to be fulfilled)
-  // get one pizza by id
-  getPizzaById({ params }, res) {
-    Pizza.findOne({ _id: params.id })
+  // get all pizzas
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
@@ -22,40 +17,50 @@ const pizzaController = {
       });
   },
 
-// createPizza (POST /api/pizzas method for handling to add a pizza to the DB)
-createPizza({ body }, res) {
-    Pizza.create(body)
+  // get one pizza by id
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
       .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => res.status(400).json(err));
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
   },
 
-// update pizza by id  (the method for updating a pizza when we make a request to PUT /api/pizzas/:id)
-updatePizza({ params, body }, res) {
-  Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
-    .then(dbPizzaData => {
-      if (!dbPizzaData) {
-        res.status(404).json({ message: 'No pizza found with this id!' });
-        return;
-      }
-      res.json(dbPizzaData);
-    })
-    .catch(err => res.status(400).json(err));
-},
+  // createPizza
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
+  },
 
-// delete pizza (method to delete a pizza from the database when we make a request to DELETE /api/pizzas/:id)
-deletePizza({ params }, res) {
-  Pizza.findOneAndDelete({ _id: params.id })
-    .then(dbPizzaData => {
-      if (!dbPizzaData) {
-        res.status(404).json({ message: 'No pizza found with this id!' });
-        return;
-      }
-      res.json(dbPizzaData);
+  // update pizza by id
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true
     })
-    .catch(err => res.status(400).json(err));
-}
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
 
+  // delete pizza
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
   }
-
+};
 
 module.exports = pizzaController;
--- models/pizza.js
+++ models/pizza.js
@@ -1,35 +1,55 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-//Pizza Hunt wants the following data to be stored when users create a new pizza
-// 1 - Name of the Pizza
-// 2 - Name of the user that created the pizza
-// 3 - A timestamp of when the pizza was created -- If no value is provided in this field when the user creates new data, the Date.now function will be executed and will provide a timestamp
-// 3* - A timestamp of any updates to the pizza's data
-// 4 - The pizza's suggested size
-// 5 - The pizza's toppings
-
-
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
     },
     size: {
       type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: 'Large'
     },
-    toppings: []
-  });
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
 
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
+});
 
-// create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
-// export the Pizza model
 module.exports = Pizza;
